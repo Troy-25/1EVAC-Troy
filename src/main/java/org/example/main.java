991d0faab3b8c03699f443fdbef99503484fbc17
TroyPineda <trontroyano25@gmail.com>
2025-11-24 09:12:51 +0100

    He extraido las constantes en  1TP_CONST

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class main {
 
+    public static final String AGENDA = "\n=== AGENDA ===";
+    public static final String LISTAR = "1. Listar";
+    public static final String BUSCAR_POR_DÍA = "2. Buscar por día";
+    public static final String BUSCAR_POR_NOMBRE = "3. Buscar por nombre";
+    public static final String AÑADIR = "4. Añadir";
+    public static final String ELIMINAR = "5. Eliminar";
+    public static final String SALIR = "6. Salir";
+    public static final String DÍA = "Día: ";
+    public static final String NOMBRE = "Nombre: ";
+
     public static void main(String[] args) {
 
         repo_file Repo = new repo_file();
@@ -11,13 +21,13 @@ public class main {
         boolean Salir = false;
 
         while (!Salir) {
-            System.out.println("\n=== AGENDA ===");
-            System.out.println("1. Listar");
-            System.out.println("2. Buscar por día");
-            System.out.println("3. Buscar por nombre");
-            System.out.println("4. Añadir");
-            System.out.println("5. Eliminar");
-            System.out.println("6. Salir");
+            System.out.println(AGENDA);
+            System.out.println(LISTAR);
+            System.out.println(BUSCAR_POR_DÍA);
+            System.out.println(BUSCAR_POR_NOMBRE);
+            System.out.println(AÑADIR);
+            System.out.println(ELIMINAR);
+            System.out.println(SALIR);
 
             String Opcion = Sc.nextLine();
 
@@ -29,14 +39,14 @@ public class main {
                     break;
 
                 case "2":
-                    System.out.print("Día: ");
+                    System.out.print(DÍA);
                     for (cumple_agenda c : Repo.buscar_dia(Integer.parseInt(Sc.nextLine()))) {
                         System.out.println(c.get_nombre());
                     }
                     break;
 
                 case "3":
-                    System.out.print("Nombre: ");
+                    System.out.print(NOMBRE);
                     cumple_agenda c = Repo.buscar_nombre(Sc.nextLine());
                     if (c != null) System.out.println(c.get_nombre());
                     break;
